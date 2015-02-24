656ba60076aae0e7f36d321f3392df3a1b8a6a35
DanRamage <dan@inlet.geol.sc.edu>
2015-02-24 10:41:59 -0500

    put exception handling around soap request.

--- commonfiles/NOAATideData.py
+++ commonfiles/NOAATideData.py
@@ -55,8 +55,9 @@ class noaaTideData(object):
       shift = 0
     else:
       shift = 1
-    
+
     data = soapClient.service.getWaterLevelRawSixMin(station, beginDate, endDate, datum, unit, shift)
+
     return(data)
                       
   def calcTideRange(self,           
@@ -77,229 +78,232 @@ class noaaTideData(object):
     tideData['HH'] = None
     tideData['L'] = None
     tideData['H'] = None
-    
-    wlData = self.getWaterLevelRawSixMinuteData(beginDate, endDate, station, datum, units, timezone)
-          
-    
-    smoothDataROC = array.array('d')
-    rawDataROC = array.array('d')
-    expSmoothedData =  array.array('d')
-    #tidePts = array.array('d')
-    #timePts = array.array('d')
-    dataLen = len(wlData.item)
-    ndx = 0
-    alpha = 0.5
-    utc_tz = pytz_timezone('UTC')
-    while ndx < dataLen:
-      
-      valN = wlData.item[ndx]['WL']
-      #tidePts.append(valN)
-      #data_ts = utc_tz.localize(datetime.strptime(wlData.item[ndx]['timeStamp'], '%Y-%m-%d %H:%M:%S.0'))
-      #timePts.append(int(get_utc_epoch(data_ts)))
-      #Then the formula for each successive point is (alpha * Xn) + (1-alpha) * Yn-1
-      #X is the original data, Yn-1 is the last smoothed data point, alpha is the smoothing constant.
-      if(ndx == 0):
-        expSmoothedData.append(valN)
-        tideMin1 = valN
-        tideMax1 = valN
-        tideMin2 = valN
-        tideMax2 = valN
-       
-      else:
-        timeStruct = utc_tz.localize(datetime.strptime(wlData.item[ndx]['timeStamp'], '%Y-%m-%d %H:%M:%S.0'))
-        timeN = int(get_utc_epoch(timeStruct))
-        timeStruct = utc_tz.localize(datetime.strptime(wlData.item[ndx-1]['timeStamp'], '%Y-%m-%d %H:%M:%S.0'))
-        timeN1 = int(get_utc_epoch(timeStruct))
-        #timeStruct = utc_tz.localize(time.strptime(wlData.item[ndx]['timeStamp'], '%Y-%m-%d %H:%M:%S.0'))
-        #timeN = time.mktime(timeStruct)
-        #timeStruct = utc_tz.localize(time.strptime(wlData.item[ndx-1]['timeStamp'], '%Y-%m-%d %H:%M:%S.0'))
-        #timeN1 = time.mktime(timeStruct)
+    try:
+      wlData = self.getWaterLevelRawSixMinuteData(beginDate, endDate, station, datum, units, timezone)
+    except suds.Webfault, e:
+      if self.logger:
+        self.logger.exception(e)
+    else:
 
-        #For each N+1 we now use the formula.
-        Yn = (alpha * wlData.item[ndx]['WL']) + ((1 - alpha) * expSmoothedData[ndx-1])
-        expSmoothedData.append(Yn)
-        
-        smoothDataROC.append((expSmoothedData[ndx] - expSmoothedData[ndx-1]) / (timeN - timeN1))
-
-        #Calcuate the rateofchange
-        #ROC for the raw data.
-        valN1 = wlData.item[ndx-1]['WL']        
-        rawDataROC.append((valN - valN1) / (timeN - timeN1))
-      ndx += 1
-    
-    ndx = 0    
-    a = None
-    b = None
-    c = None
-    dirChangeCnt = 0
-    chordLen = 10
-    midPt = chordLen / 2
-    ptFound = False
-    stopProc = False
-    dataLen = len(wlData.item)
-    slopePositive = False
-
-    #plt.plot(timePts,tidePts,'o', x_new, y_new)
-    ##plt.xlim([timePts[0]-1, timePts[-1] + 1 ])
-    #plt.savefig('/users/danramage/tmp/out.png', dpi=96)
-
-    if(self.logger != None):
-      self.logger.info("Checking Raw data.")
-    
-    tideChange = None
-    changeNdx = None
-    lastSlope = None
-    for ndx in range(0, len(wlData.item)):
-      a = wlData.item[ndx]['WL']
-      timeStamp = wlData.item[ndx].timeStamp
-      if ndx + chordLen < dataLen - 1:
-        c = wlData.item[ndx+chordLen]['WL']
-      else:
-        stopProc = True
-      if tideChange is None:
-        tideChange = a
-        tide_change_ts = timeStamp
-      if not stopProc:
-        #Calc slope
-        #Ascending
-        if c - a > 0:
-          if lastSlope == 0:
-            if tideData['LL'] is None:
-              #tideData['LL'] = tideChange
-              tideData['LL'] = { 
-                                'value' : tideChange,
-                                'date' : tide_change_ts
-                               }
-            elif tideChange < tideData['LL']['value']:
-              tmp = tideData['LL']
-              #tideData['LL'] = tideChange
-              tideData['LL'] = { 
-                                'value' : tideChange,
-                                'date' : tide_change_ts
-                               }
-              tideData['L'] = tmp
-            else:
-              tideData['L'] = { 
-                                'value' : tideChange,
-                                'date' : tide_change_ts
-                               }
-              
-            #print("Tide Min at: %f@%s" %(tideChange,timeStamp))
-            if self.logger:
-              self.logger.debug("Tide Min at: %f@%s" %(tideChange,tide_change_ts))
-            #Found the max tide, so another is not going to occur any quicker than the chord length, so increment the ndx.
-            ndx += chordLen
-            #Slope has changed direction.
-            lastSlope = 1
-            continue
-          lastSlope = 1
-          
-          if(a > tideChange):
-            tideChange = a
-            tide_change_ts = timeStamp
-            changeNdx = ndx
-        #Descending
-        elif c - a < 0:
-          if lastSlope == 1:
-            if tideData['HH'] is None:
-              #tideData['HH'] = tideChange
-              tideData['HH'] = { 
-                                'value' : tideChange,
-                                'date' : tide_change_ts
-                               }
-              
-            elif tideChange > tideData['HH']['value']:
-              tmp = tideData['HH']
-              #tideData['HH'] = tideChange
-              tideData['HH'] = { 
-                                'value' : tideChange,
-                                'date' : tide_change_ts
-                               }              
-              tideData['H'] = tmp
-            else:
-              tideData['H'] = { 
-                                'value' : tideChange,
-                                'date' : tide_change_ts
-                               }
-            
-            #print("Tide Max at: %f@%s" %(tideChange,timeStamp))
-            if self.logger:
-              self.logger.debug("Tide Max at: %f@%s" %(tideChange,tide_change_ts))
-            
-            #Found the max tide, so another is not going to occur any quicker than the chord length, so increment the ndx.
-            ndx += chordLen
-            #Slope has changed direction.
-            lastSlope = 0
-            continue            
-          lastSlope = 0
-          
-          if a < tideChange:
-            tideChange = a
-            tide_change_ts = timeStamp
-
-            changeNdx = ndx
-            
-      ndx += 1
-    
-    if smoothData:
-      print("Checking smoothed data.")
-      dataLen = len(expSmoothedData)
+      smoothDataROC = array.array('d')
+      rawDataROC = array.array('d')
+      expSmoothedData =  array.array('d')
+      #tidePts = array.array('d')
+      #timePts = array.array('d')
+      dataLen = len(wlData.item)
       ndx = 0
+      alpha = 0.5
+      utc_tz = pytz_timezone('UTC')
+      while ndx < dataLen:
+
+        valN = wlData.item[ndx]['WL']
+        #tidePts.append(valN)
+        #data_ts = utc_tz.localize(datetime.strptime(wlData.item[ndx]['timeStamp'], '%Y-%m-%d %H:%M:%S.0'))
+        #timePts.append(int(get_utc_epoch(data_ts)))
+        #Then the formula for each successive point is (alpha * Xn) + (1-alpha) * Yn-1
+        #X is the original data, Yn-1 is the last smoothed data point, alpha is the smoothing constant.
+        if(ndx == 0):
+          expSmoothedData.append(valN)
+          tideMin1 = valN
+          tideMax1 = valN
+          tideMin2 = valN
+          tideMax2 = valN
+
+        else:
+          timeStruct = utc_tz.localize(datetime.strptime(wlData.item[ndx]['timeStamp'], '%Y-%m-%d %H:%M:%S.0'))
+          timeN = int(get_utc_epoch(timeStruct))
+          timeStruct = utc_tz.localize(datetime.strptime(wlData.item[ndx-1]['timeStamp'], '%Y-%m-%d %H:%M:%S.0'))
+          timeN1 = int(get_utc_epoch(timeStruct))
+          #timeStruct = utc_tz.localize(time.strptime(wlData.item[ndx]['timeStamp'], '%Y-%m-%d %H:%M:%S.0'))
+          #timeN = time.mktime(timeStruct)
+          #timeStruct = utc_tz.localize(time.strptime(wlData.item[ndx-1]['timeStamp'], '%Y-%m-%d %H:%M:%S.0'))
+          #timeN1 = time.mktime(timeStruct)
+
+          #For each N+1 we now use the formula.
+          Yn = (alpha * wlData.item[ndx]['WL']) + ((1 - alpha) * expSmoothedData[ndx-1])
+          expSmoothedData.append(Yn)
+
+          smoothDataROC.append((expSmoothedData[ndx] - expSmoothedData[ndx-1]) / (timeN - timeN1))
+
+          #Calcuate the rateofchange
+          #ROC for the raw data.
+          valN1 = wlData.item[ndx-1]['WL']
+          rawDataROC.append((valN - valN1) / (timeN - timeN1))
+        ndx += 1
+
+      ndx = 0
+      a = None
+      b = None
+      c = None
+      dirChangeCnt = 0
+      chordLen = 10
+      midPt = chordLen / 2
       ptFound = False
       stopProc = False
-      
-      while ndx < dataLen:
-        a = expSmoothedData[ndx]
-        if ndx + midPt < dataLen - 1:
-          b = expSmoothedData[ndx+midPt]
-        else:
-          stopProc = True
-        if ndx + chordLen < dataLen - 1:
-          c = expSmoothedData[ndx+chordLen]
-        else:
-          stopProc = True
-        if stopProc == False:
-          #Calc slope
-          if c - a > 0:
-            if b > a and b > c:
-              #print("Tide change at Ndx: %d Val: %f" %(ndx+midPt, b))
-              if self.logger != None:
-                self.logger.debug("Tide change at Ndx: %d Val: %f" %(ndx+midPt, b))
-              
-              ptFound = True
-          elif c - a < 0:
-            if b < a and b < c:
-              #print("Tide change at Ndx: %d Val: %f" %(ndx+midPt, b))
-              if self.logger:
-                self.logger.debug("Tide change at Ndx: %d Val: %f" %(ndx+midPt, b))
-              ptFound = True
-        if ptFound == False:
-          ndx += 1
-        else:
-          ndx = ndx+midPt
-          ptFound = False
-
-    if tideFileDir != None:
-      filename = "%s\\%s-%s.csv" %(tideFileDir,beginDate,endDate)
-      tideFile = open(filename, "w")
-      
-      ndx = 0
       dataLen = len(wlData.item)
-      while ndx < dataLen:
-        timeStruct = time.strptime(wlData.item[ndx]['timeStamp'], '%Y-%m-%d %H:%M:%S.0')
-        seconds = time.mktime(timeStruct)
-        medianROC = ''      
-        rawROC = ''
-        smoothedData = ''
-        if(ndx < len(rawDataROC)):
-          rawROC = rawDataROC[ndx]
-          smoothedROC = smoothDataROC[ndx]
-          smoothedData = expSmoothedData[ndx]
-        
-        outbuf = "%s,%s,%s,%s,%s\n" %(seconds,wlData.item[ndx]['WL'], rawROC, smoothedData, smoothedROC)
-        ndx += 1 
-        tideFile.write(outbuf)
-      tideFile.close()
+      slopePositive = False
+
+      #plt.plot(timePts,tidePts,'o', x_new, y_new)
+      ##plt.xlim([timePts[0]-1, timePts[-1] + 1 ])
+      #plt.savefig('/users/danramage/tmp/out.png', dpi=96)
+
+      if(self.logger != None):
+        self.logger.info("Checking Raw data.")
+
+      tideChange = None
+      changeNdx = None
+      lastSlope = None
+      for ndx in range(0, len(wlData.item)):
+        a = wlData.item[ndx]['WL']
+        timeStamp = wlData.item[ndx].timeStamp
+        if ndx + chordLen < dataLen - 1:
+          c = wlData.item[ndx+chordLen]['WL']
+        else:
+          stopProc = True
+        if tideChange is None:
+          tideChange = a
+          tide_change_ts = timeStamp
+        if not stopProc:
+          #Calc slope
+          #Ascending
+          if c - a > 0:
+            if lastSlope == 0:
+              if tideData['LL'] is None:
+                #tideData['LL'] = tideChange
+                tideData['LL'] = {
+                                  'value' : tideChange,
+                                  'date' : tide_change_ts
+                                 }
+              elif tideChange < tideData['LL']['value']:
+                tmp = tideData['LL']
+                #tideData['LL'] = tideChange
+                tideData['LL'] = {
+                                  'value' : tideChange,
+                                  'date' : tide_change_ts
+                                 }
+                tideData['L'] = tmp
+              else:
+                tideData['L'] = {
+                                  'value' : tideChange,
+                                  'date' : tide_change_ts
+                                 }
+
+              #print("Tide Min at: %f@%s" %(tideChange,timeStamp))
+              if self.logger:
+                self.logger.debug("Tide Min at: %f@%s" %(tideChange,tide_change_ts))
+              #Found the max tide, so another is not going to occur any quicker than the chord length, so increment the ndx.
+              ndx += chordLen
+              #Slope has changed direction.
+              lastSlope = 1
+              continue
+            lastSlope = 1
+
+            if(a > tideChange):
+              tideChange = a
+              tide_change_ts = timeStamp
+              changeNdx = ndx
+          #Descending
+          elif c - a < 0:
+            if lastSlope == 1:
+              if tideData['HH'] is None:
+                #tideData['HH'] = tideChange
+                tideData['HH'] = {
+                                  'value' : tideChange,
+                                  'date' : tide_change_ts
+                                 }
+
+              elif tideChange > tideData['HH']['value']:
+                tmp = tideData['HH']
+                #tideData['HH'] = tideChange
+                tideData['HH'] = {
+                                  'value' : tideChange,
+                                  'date' : tide_change_ts
+                                 }
+                tideData['H'] = tmp
+              else:
+                tideData['H'] = {
+                                  'value' : tideChange,
+                                  'date' : tide_change_ts
+                                 }
+
+              #print("Tide Max at: %f@%s" %(tideChange,timeStamp))
+              if self.logger:
+                self.logger.debug("Tide Max at: %f@%s" %(tideChange,tide_change_ts))
+
+              #Found the max tide, so another is not going to occur any quicker than the chord length, so increment the ndx.
+              ndx += chordLen
+              #Slope has changed direction.
+              lastSlope = 0
+              continue
+            lastSlope = 0
+
+            if a < tideChange:
+              tideChange = a
+              tide_change_ts = timeStamp
+
+              changeNdx = ndx
+
+        ndx += 1
+
+      if smoothData:
+        print("Checking smoothed data.")
+        dataLen = len(expSmoothedData)
+        ndx = 0
+        ptFound = False
+        stopProc = False
+
+        while ndx < dataLen:
+          a = expSmoothedData[ndx]
+          if ndx + midPt < dataLen - 1:
+            b = expSmoothedData[ndx+midPt]
+          else:
+            stopProc = True
+          if ndx + chordLen < dataLen - 1:
+            c = expSmoothedData[ndx+chordLen]
+          else:
+            stopProc = True
+          if stopProc == False:
+            #Calc slope
+            if c - a > 0:
+              if b > a and b > c:
+                #print("Tide change at Ndx: %d Val: %f" %(ndx+midPt, b))
+                if self.logger != None:
+                  self.logger.debug("Tide change at Ndx: %d Val: %f" %(ndx+midPt, b))
+
+                ptFound = True
+            elif c - a < 0:
+              if b < a and b < c:
+                #print("Tide change at Ndx: %d Val: %f" %(ndx+midPt, b))
+                if self.logger:
+                  self.logger.debug("Tide change at Ndx: %d Val: %f" %(ndx+midPt, b))
+                ptFound = True
+          if ptFound == False:
+            ndx += 1
+          else:
+            ndx = ndx+midPt
+            ptFound = False
+
+      if tideFileDir != None:
+        filename = "%s\\%s-%s.csv" %(tideFileDir,beginDate,endDate)
+        tideFile = open(filename, "w")
+
+        ndx = 0
+        dataLen = len(wlData.item)
+        while ndx < dataLen:
+          timeStruct = time.strptime(wlData.item[ndx]['timeStamp'], '%Y-%m-%d %H:%M:%S.0')
+          seconds = time.mktime(timeStruct)
+          medianROC = ''
+          rawROC = ''
+          smoothedData = ''
+          if(ndx < len(rawDataROC)):
+            rawROC = rawDataROC[ndx]
+            smoothedROC = smoothDataROC[ndx]
+            smoothedData = expSmoothedData[ndx]
+
+          outbuf = "%s,%s,%s,%s,%s\n" %(seconds,wlData.item[ndx]['WL'], rawROC, smoothedData, smoothedROC)
+          ndx += 1
+          tideFile.write(outbuf)
+        tideFile.close()
     
     return(tideData)
 if __name__ == '__main__':
