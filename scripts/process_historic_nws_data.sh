2557177a7199ec4d41fea5269d4095b93dbb369a
DanRamage <dan@inlet.geol.sc.edu>
2015-07-29 08:02:52 -0400

    Init checkin

---
+++ scripts/process_historic_nws_data.sh
@@ -0,0 +1,5 @@
+source /usr2/virtualenvs/pyenv2.7/bin/activate
+
+
+python florida_wq_import_historical_data.py --ConfigFile=/home/xeniaprod/scripts/Florida-Water-Quality/config/florida_config.ini --NWSDirectory=/home/xeniaprod/scripts/Florida-Water-Quality/data/in-situ/nws-ksrq/months
+
