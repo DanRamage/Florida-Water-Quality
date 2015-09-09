8135d36b5e508c45aebb5690ba66404c5b430f9e
DanRamage <dan@inlet.geol.sc.edu>
2015-09-09 14:54:07 -0400

    Init checkin.

--- scripts/build_bacteria_csv.py
+++ scripts/build_bacteria_csv.py
@@ -7,6 +7,7 @@ import optparse
 import ConfigParser
 import simplejson as json
 from florida_wq_data import florida_sample_sites
+from datetime import datetime
 
 def main():
   parser = optparse.OptionParser()
@@ -14,6 +15,8 @@ def main():
                     help="INI Configuration file." )
   parser.add_option("-o", "--CSVOutFile", dest="out_filename", default=None,
                     help="CSV file to write results." )
+  parser.add_option("-d", "--DatesFile", dest="dates_file", default=None,
+                    help="File to write all the unique test dates.")
 
   (options, args) = parser.parse_args()
 
@@ -41,6 +44,7 @@ def main():
     fl_sites.load_sites(file_name=sites_location_file, boundary_file=boundaries_location_file)
 
     try:
+      unique_dates = []
       for site in fl_sites:
         out_filename = os.path.join(options.out_filename, '%s.csv' % (site.name))
         if logger:
@@ -58,9 +62,19 @@ def main():
                     csv_out_file.write("%s,%f\n" % (advisory['date'], advisory['value'][0]))
                   else:
                     csv_out_file.write("%s,%f\n" % (advisory['date'], advisory['value']))
+                  sample_date = datetime.strptime(advisory['date'], '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%dT00:00:00')
+                  if sample_date not in unique_dates:
+                    unique_dates.append(sample_date)
+
             except IOError,e:
               if logger:
                 logger.exception(e)
+
+      if len(unique_dates):
+        with open(options.dates_file, 'w') as dates_file_obj:
+          unique_dates.sort()
+          dates_file_obj.write((','.join(unique_dates)))
+
     except IOError, e:
       if logger:
         logger.exception(e)
