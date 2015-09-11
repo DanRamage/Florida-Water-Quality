e3de46bfaff376c34300b656efc5f62d45667dce
DanRamage <dan@inlet.geol.sc.edu>
2015-09-11 11:15:43 -0400

    init checkin.

---
+++ scripts/florida_wq_prediction_engine.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /usr2/virtualenvs/pyenv2.7/bin/activate
+
+python florida_wq_prediction_engine.py --ConfigFile=/home/xeniaprod/scripts/Florida-Water-Quality/config/florida_prediction_config.ini > /home/xeniaprod/tmp/log/florida_wq_prediction_engine_sh.log 2>&1
