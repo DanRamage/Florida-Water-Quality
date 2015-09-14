4589e2637573d548c2340faa2c5798d07e889e02
DanRamage <dan@inlet.geol.sc.edu>
2015-09-14 10:48:02 -0400

    Changes to get the cron job working correctly

--- scripts/florida_wq_prediction_engine.sh
+++ scripts/florida_wq_prediction_engine.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-source /usr2/virtualenvs/pyenv2.7/bin/activate
+source /usr2/virtualenvs/pyenv2.7/bin/activate;
+
+cd /home/xeniaprod/scripts/Florida-Water-Quality/scripts;
 
 python florida_wq_prediction_engine.py --ConfigFile=/home/xeniaprod/scripts/Florida-Water-Quality/config/florida_prediction_config.ini > /home/xeniaprod/tmp/log/florida_wq_prediction_engine_sh.log 2>&1
