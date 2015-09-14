e5934d6bc0f9b3c214e2ded155cc16354747c423
DanRamage <dan@inlet.geol.sc.edu>
2015-09-14 10:52:09 -0400

    Check for new bacteria sample data script added.

--- scripts/florida_wq_prediction_engine.sh
+++ scripts/florida_wq_prediction_engine.sh
@@ -4,4 +4,6 @@ source /usr2/virtualenvs/pyenv2.7/bin/activate;
 
 cd /home/xeniaprod/scripts/Florida-Water-Quality/scripts;
 
-python florida_wq_prediction_engine.py --ConfigFile=/home/xeniaprod/scripts/Florida-Water-Quality/config/florida_prediction_config.ini > /home/xeniaprod/tmp/log/florida_wq_prediction_engine_sh.log 2>&1
+python florida_wq_harvest_bacteria_data.py --ConfigFile=/home/xeniaprod/scripts/Florida-Water-Quality/config/florida_wq_harvest_bacteria_data.ini > /home/xeniaprod/tmp/log/florida_wq_prediction_engine_sh.log 2>&1
+
+python florida_wq_prediction_engine.py --ConfigFile=/home/xeniaprod/scripts/Florida-Water-Quality/config/florida_prediction_config.ini >> /home/xeniaprod/tmp/log/florida_wq_prediction_engine_sh.log 2>&1
