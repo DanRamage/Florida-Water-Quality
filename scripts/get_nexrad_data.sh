8ce8677bda839e756fbcb552400565364475a451
DanRamage <dan@inlet.geol.sc.edu>
2015-11-25 08:05:01 -0500

    ADd full path to the wqXMRGProcessing file.

--- scripts/get_nexrad_data.sh
+++ scripts/get_nexrad_data.sh
@@ -2,4 +2,4 @@
 
 source /usr2/virtualenvs/pyenv2.7/bin/activate
 
-python wqXMRGProcessing.py --ConfigFile=/home/xeniaprod/scripts/Florida-Water-Quality/config/florida_config.ini --FillGaps --BackfillNHours=192
+python /home/xeniaprod/scripts/Florida-Water-Quality/commonfiles/python/wqXMRGProcessing.py --ConfigFile=/home/xeniaprod/scripts/Florida-Water-Quality/config/florida_config.ini --FillGaps --BackfillNHours=192
