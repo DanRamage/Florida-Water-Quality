de1201dbb7f84eecedcaae1db46466ec50164097
DanRamage <ChiefDan@gmail.com>
2016-11-30 09:34:45 -0500

    Use new calctide call.

--- scripts/florida_wq_data.py
+++ scripts/florida_wq_data.py
@@ -17,7 +17,7 @@ from wqHistoricalData import wq_data
 from wqXMRGProcessing import wqDB
 from wqHistoricalData import station_geometry,sampling_sites, wq_defines, geometry_list
 from date_time_utils import get_utc_epoch
-from NOAATideData import noaaTideData
+from NOAATideData import noaaTideDataExt
 from xeniaSQLAlchemy import xeniaAlchemy, multi_obs
 from stats import calcAvgSpeedAndDir
 from romsTools import closestCellFromPtInPolygon
@@ -515,7 +515,8 @@ class florida_wq_historical_data(wq_data):
       tide_start_time = (start_date - timedelta(hours=24))
       tide_end_time = start_date
 
-      tide = noaaTideData(use_raw=True, logger=self.logger)
+      #tide = noaaTideData(use_raw=True, logger=self.logger)
+      tide = noaaTideDataExt(use_raw=True, logger=self.logger)
       #Date/Time format for the NOAA is YYYYMMDD
 
       try:
