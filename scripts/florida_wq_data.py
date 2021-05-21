a0918e6b3e471736b96b5f4f46db9d2dc147d059
DANRAMAGE <dan@inlet.geol.sc.edu>
2021-05-21 10:42:08 -0400

    New plugin logging changes.

--- scripts/florida_wq_data.py
+++ scripts/florida_wq_data.py
@@ -939,8 +939,8 @@ class florida_wq_model_data(florida_wq_historical_data):
     self.get_nexrad_data(start_date, wq_tests_data)
     #self.get_hycom_model_data(start_date, wq_tests_data)
 
-    for key in wq_tests_data:
-      self.logger.debug("%s: %s" % (key, wq_tests_data[key]))
+    #for key in wq_tests_data:
+    #  self.logger.debug("%s: %s" % (key, wq_tests_data[key]))
 
     if self.logger:
       self.logger.debug("Site: %s Finished query data for datetime: %s" % (self.site.name, start_date))
