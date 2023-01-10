a01814ad6eb61b6d456ad5738488a140fc87fee3
DANRAMAGE <ChiefDan@gmail.com>
2023-01-10 11:59:18 -0500

    Added processing for the new sampling data feed.

--- scripts/harvest_bacteria_data.sh
+++ scripts/harvest_bacteria_data.sh
@@ -4,4 +4,4 @@ source /usr/local/virtualenv/pyenv-3.8.5/bin/activate
 
 cd /home/xeniaprod/scripts/Florida-Water-Quality/scripts;
 
-python florida_wq_harvest_bacteria_data.py --ConfigFile=/home/xeniaprod/scripts/Florida-Water-Quality/config/florida_wq_harvest_bacteria_data.ini --SiteAliases=/home/xeniaprod/scripts/Florida_Water_Quality/config/all_data_sites_mapping.json > /home/xeniaprod/tmp/log/florida_harvest_bacteria_data_sh.log 2>&1
+python florida_wq_harvest_bacteria_data.py --ConfigFile=/home/xeniaprod/scripts/Florida-Water-Quality/config/florida_wq_harvest_bacteria_data.ini --SiteAliases=/home/xeniaprod/scripts/Florida-Water-Quality/config/all_data_sites_mapping.json > /home/xeniaprod/tmp/log/florida_harvest_bacteria_data_sh.log 2>&1
